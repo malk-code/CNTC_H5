97bb6260377fb068a62db90ed2658b3e9ab4bd84
徐楠峰 <2893096286@qq.com>
2022-02-16 18:05:28 +0800

    add

--- src/pages/AnswerPages/index.tsx
+++ src/pages/AnswerPages/index.tsx
@@ -6,7 +6,7 @@
  * @description 答题页面
  */
 import { Component } from 'react'
-import { View, Text, Image } from '@tarojs/components'
+import { View, Text, Image, ScrollView } from '@tarojs/components'
 import './index.less'
 
 const IMAGEURL = require('../../assets/images/cntc_top.png')
@@ -35,6 +35,33 @@ export default class AnswerPagesIndex extends Component<any, any> {
 					className='answer_pages_topImages'
 					src={IMAGEURL}
 				/>
+				<View className='answer_pages_container'>
+					{/* 倒计时 */}
+					<View className='answer_pages_notescontainer'>
+						<View className='answer_pages_notescontainer_pointer'></View>
+						<Text className='answer_pages_notescontainer_notes'>本评价表填写时间为10分钟，倒计时9:59</Text>
+					</View>
+
+					{/* 信息 */}
+					<View className='answer_pages_infocontainer'>
+						<View>
+							<Text>评价人员:</Text>
+							<Text>xxxx</Text>
+						</View>
+						<View>
+							<Text>评吸日期:</Text>
+							<Text>2022年1月17日</Text>
+						</View>
+						<View>
+							<Text>对比样生产信息:</Text>
+							<Text></Text>
+						</View>
+						<View>
+							<Text>评价样生产信息:</Text>
+							<Text>xxxx</Text>
+						</View>
+					</View>
+				</View>
 			</View>
 		)
 	}
